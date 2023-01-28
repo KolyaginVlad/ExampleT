487f53a4d83207055498cc7c561634a11498f0b8
bsc-smarchinskaya <ext_smarchinskaya@rencredit.ru>
2023-01-28 23:06:00 +0300

    add push messages handling

--- app/src/main/java/com/example/examplet/ui/Application.kt
+++ app/src/main/java/com/example/examplet/ui/Application.kt
@@ -1,6 +1,20 @@
 package com.example.examplet.ui
 
+import android.util.Log
+import com.google.firebase.messaging.FirebaseMessaging
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class Application : android.app.Application()
+class Application : android.app.Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        FirebaseMessaging.getInstance().token.addOnCompleteListener { task->
+            if(!task.isSuccessful){
+                return@addOnCompleteListener
+            }
+            val token = task.result
+            Log.d("TAG","Token $token")
+        }
+    }
+}
